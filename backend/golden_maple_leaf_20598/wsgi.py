2ff1f2ea3c459543b26db05354c5f7fa34953cb4
Crowdbotics <team@crowdbotics.com>
2020-09-23 05:27:30 +0000

    Update models

--- backend/golden_maple_leaf_20598/wsgi.py
+++ backend/golden_maple_leaf_20598/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'golden_maple_leaf_20598.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "golden_maple_leaf_20598.settings")
 
 application = get_wsgi_application()
